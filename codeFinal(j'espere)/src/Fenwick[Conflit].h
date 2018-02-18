ae17f937e94ac9e2f15b761f33179000e1bb83fe
nathanmutin <32647745+nathanmutin@users.noreply.github.com>
2018-02-18 16:56:10 +0100

    code sans pid

---
+++ codeFinal(j'espere)/src/Fenwick[Conflit].h
@@ -0,0 +1,54 @@
+/*
+ * Fenwick.h
+ *
+ *  Created on: 10 févr. 2018
+ *      Author: REBAUDET Thomas
+ */
+
+#ifndef FENWICK_H_
+#define FENWICK_H_
+#include <VictorSP.h>
+#include "WPILib.h"
+#include <Joystick.h>
+#include <GenericHID.h>
+#include <Encoder.h>
+#include <Servo.h>
+#include <Pince.h>
+
+
+namespace std {
+
+class Fenwick {
+public:
+	void setConsigne(int consigne);
+	void deplacerFenwick();
+	void ajuster(int pov);
+	void desactiverServo();
+	void monteeFinaleFenwick(/*Pince* pince*/);
+	void monteeDuRobot();
+	void activerServo();
+	void afficherPosition();
+	Fenwick();
+	virtual ~Fenwick();
+
+private:
+	VictorSP* fenwick1;
+	VictorSP* fenwick2;
+	VictorSP* fenwick3;
+	Servo* servo;
+	Encoder* Encodeur;
+
+
+	double consigne, position, erreur, tolerance;
+	double kP = 10; //3 pour le 1er etage
+	double kI = 0.1;
+	double kD = 0;
+	double sommeErreurs = 0;
+	double differenceErreurs = 0;
+	double puissanceMoteur = 0;
+	double erreurPrecedente;
+};
+
+}
+
+#endif /* FENWICK_H_ */
